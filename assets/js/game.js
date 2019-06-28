64b75c4aacb34aebc8f87393c7eb79c716cf8edb
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-28 19:10:07 -0400

    added initial music and initial set up of buttons and directions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
+    //hide until game has begun
+    $("#choose-text").html("");
+    $("#challenger-text").html("");
+    $("#wizard-text").html("");
+    $("#expelliarmus").hide();
 
-    //Variables
+    // let initialMusic = new Audio("assets/sound/Prologue.mp3");
+    // initialMusic.play();
 
-    
+    $("#begin-game").on("click", function() {
+        $("#directions").html("");
+        $(this).hide();
+        $("#expelliarmus").show();
 
+    });
 });
+
+function initialize () {
+    
+}
