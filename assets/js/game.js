3d7d92deeb8d9439460106792b72a57fd81b33ba
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-03 14:40:01 -0400

    updated with on click functions for each character so that ap and cap are assigned properly

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,47 @@ let expelliarmusSound = new Audio('assets/sound/expelliarmus.mp3');
 let wizardChosen = false; 
 let challengerChosen = false; 
 
-let wizardOptions = [
-    {name: "Harry Potter", health: 175, attackPower: 15, counterAttackPower: 25}, 
-    {name: "Hermione Granger", health: 180, attackPower: 12, counterAttackPower: 22},
-    {name: "Lord Voldemort", health: 120, attackPower: 10, counterAttackPower: 20},
-    {name: "Severus Snape", health: 110, attackPower: 8, counterAttackPower: 18}
-    ]; 
+let attackValue = "";
+let counterAttackValue = "";
+// let wizardOptions = [
+//     {name: "Harry Potter", health: 175, attackPower: 15, counterAttackPower: 25}, 
+//     {name: "Hermione Granger", health: 180, attackPower: 12, counterAttackPower: 22},
+//     {name: "Lord Voldemort", health: 120, attackPower: 10, counterAttackPower: 20},
+//     {name: "Severus Snape", health: 110, attackPower: 8, counterAttackPower: 18}
+//     ]; 
+
+let harry = {
+    name: "Harry Potter",
+    health: 175, 
+    attackPower: 15, 
+    counterAttackPower: 25
+};
+
+let hermione = {
+    name: "Hermione Granger",
+    health: 180, 
+    attackPower: 12, 
+    counterAttackPower: 22
+};
+
+let voldemort = {
+    name: "Lord Voldemort",
+    health: 120, 
+    attackPower: 10, 
+    counterAttackPower: 20
+};
+
+let snape = {
+    name: "Severus Snape",
+    health: 110, 
+    attackPower: 8, 
+    counterAttackPower: 18
+};
 
 function initialize() {
+
+    $("#play-again").hide();
+    $("expelliarmus").show();
     
     $("#chosen-wizard").empty();
     $("#chosen-challenger").empty(); 
@@ -19,34 +52,140 @@ function initialize() {
     wizardChosen = false;
     challengerChosen = false; 
 
-    wizardOptions = [
-    {name: "Harry Potter", health: 175, attackPower: 15, counterAttackPower: 25}, 
-    {name: "Hermione Granger", health: 180, attackPower: 12, counterAttackPower: 22},
-    {name: "Lord Voldemort", health: 120, attackPower: 10, counterAttackPower: 20},
-    {name: "Severus Snape", health: 110, attackPower: 8, counterAttackPower: 18}
-    ]; 
+    attackValue = "";
+    counterAttackValue = "";
+    // wizardOptions = [
+    // {name: "Harry Potter", health: 175, attackPower: 15, counterAttackPower: 25}, 
+    // {name: "Hermione Granger", health: 180, attackPower: 12, counterAttackPower: 22},
+    // {name: "Lord Voldemort", health: 120, attackPower: 10, counterAttackPower: 20},
+    // {name: "Severus Snape", health: 110, attackPower: 8, counterAttackPower: 18}
+    // ]; 
 
-    $("#harry-health").text(wizardOptions[0].health);
-    $("#hermione-health").text(wizardOptions[1].health);
-    $("#voldemort-health").text(wizardOptions[2].health);
-    $("#snape-health").text(wizardOptions[3].health);
+    $("#harry-health").text(harry.health);
+    $("#hermione-health").text(hermione.health);
+    $("#voldemort-health").text(voldemort.health);
+    $("#snape-health").text(snape.health);
 }
     
 $(initialize());
 
-function chooseWizardChallenger() {
-    $(".wizard-selection").on("click", function() {
-        if (wizardChosen === false) {
-            $("#chosen-wizard").append($(this));
-            wizardChosen = true; 
-        } else if (wizardChosen === true) {
-            $("#chosen-challenger").append($(this));
-            challengerChosen = true; 
-        }
-    });
-}
+// function chooseWizardChallenger() {
+//     $(".wizard-selection").on("click", function() {
+//         if (wizardChosen === false) {
+//             $("#chosen-wizard").append($(this));
+//             wizardChosen = true; 
+//         } else if (wizardChosen === true) {
+//             $("#chosen-challenger").append($(this));
+//             challengerChosen = true; 
+//         }
+//     });
+// }
 
-$(chooseWizardChallenger()); 
+// $(chooseWizardChallenger()); 
+
+$("#js-harry").on("click", function() {
+    if(wizardChosen === false && challengerChosen === false) {
+        wizardChosen = true; 
+        $("#chosen-wizard").append($(this));
+        $(this).attr("w-ap", harry.attackPower);
+        $(".w-attack-power").text(harry.attackPower);
+        attackValue = ($(this).attr("w-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("w-cap", harry.counterAttackPower);
+        $(".w-counter-attack-power").text(harry.counterAttackPower);
+        counterAttackValue = ($(this).attr("w-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    } else if (wizardChosen === true && challengerChosen === false) {
+        challengerChosen = true; 
+        $("#chosen-challenger").append($(this));
+        $(this).attr("c-ap", harry.attackPower);
+        $(".c-attack-power").text(harry.attackPower);
+        attackValue = ($(this).attr("c-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("c-cap", harry.counterAttackPower);
+        $(".c-counter-attack-power").text(harry.counterAttackPower);
+        counterAttackValue = ($(this).attr("c-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    }
+});
+
+$("#js-hermione").on("click", function() {
+    if(wizardChosen === false && challengerChosen === false) {
+        wizardChosen = true; 
+        $("#chosen-wizard").append($(this));
+        $(this).attr("w-ap", hermione.attackPower);
+        $(".w-attack-power").text(hermione.attackPower);
+        attackValue = ($(this).attr("w-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("w-cap", hermione.counterAttackPower);
+        $(".w-counter-attack-power").text(hermione.counterAttackPower);
+        counterAttackValue = ($(this).attr("w-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    } else if (wizardChosen === true && challengerChosen === false) {
+        challengerChosen = true; 
+        $("#chosen-challenger").append($(this));
+        $(this).attr("c-ap", hermione.attackPower);
+        $(".c-attack-power").text(hermione.attackPower);
+        attackValue = ($(this).attr("c-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("c-cap", hermione.counterAttackPower);
+        $(".c-counter-attack-power").text(hermione.counterAttackPower);
+        counterAttackValue = ($(this).attr("c-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    }
+});
+
+$("#js-voldemort").on("click", function() {
+    if(wizardChosen === false && challengerChosen === false) {
+        wizardChosen = true; 
+        $("#chosen-wizard").append($(this));
+        $(this).attr("w-ap", voldemort.attackPower);
+        $(".w-attack-power").text(voldemort.attackPower);
+        attackValue = ($(this).attr("w-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("w-cap", voldemort.counterAttackPower);
+        $(".w-counter-attack-power").text(voldemort.counterAttackPower);
+        counterAttackValue = ($(this).attr("w-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    } else if (wizardChosen === true && challengerChosen === false) {
+        challengerChosen = true; 
+        $("#chosen-challenger").append($(this));
+        $(this).attr("c-ap", voldemort.attackPower);
+        $(".c-attack-power").text(voldemort.attackPower);
+        attackValue = ($(this).attr("c-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("c-cap", voldemort.counterAttackPower);
+        $(".c-counter-attack-power").text(voldemort.counterAttackPower);
+        counterAttackValue = ($(this).attr("c-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    }
+});
+
+$("#js-snape").on("click", function() {
+    if(wizardChosen === false && challengerChosen === false) {
+        wizardChosen = true; 
+        $("#chosen-wizard").append($(this));
+        $(this).attr("w-ap", snape.attackPower);
+        $(".w-attack-power").text(snape.attackPower);
+        attackValue = ($(this).attr("w-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("w-cap", snape.counterAttackPower);
+        $(".w-counter-attack-power").text(snape.counterAttackPower);
+        counterAttackValue = ($(this).attr("w-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    } else if (wizardChosen === true && challengerChosen === false) {
+        challengerChosen = true; 
+        $("#chosen-challenger").append($(this));
+        $(this).attr("c-ap", snape.attackPower);
+        $(".c-attack-power").text(snape.attackPower);
+        attackValue = ($(this).attr("c-ap"));
+        attackValue = parseInt(attackValue);
+        $(this).attr("c-cap", snape.counterAttackPower);
+        $(".c-counter-attack-power").text(snape.counterAttackPower);
+        counterAttackValue = ($(this).attr("c-cap"));
+        counterAttackValue = parseInt(counterAttackValue);
+    }
+});
 
 function handleExpelliarmus() {
     $("#expelliarmus").on("click", function(){
@@ -64,7 +203,7 @@ function handleExpelliarmus() {
 
 $(handleExpelliarmus());
 
-function increaseAttackPower() {
+function duel() {
     
 }
 
