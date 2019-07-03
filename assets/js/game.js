05d98292b7717b107805aa485cdc702c1ef0087d
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-03 16:40:03 -0400

    updated duel function; still working on incrementing attack value for chosen wizard

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,8 @@ let expelliarmusSound = new Audio('assets/sound/expelliarmus.mp3');
 
 let wizardChosen = false; 
 let challengerChosen = false; 
+let currentWizard = false;
+let currentChallenger = false; 
 
 let attackValue = "";
 let counterAttackValue = "";
@@ -51,15 +53,39 @@ function initialize() {
 
     wizardChosen = false;
     challengerChosen = false; 
+    currentWizard = false;
+    currentChallenger = false; 
 
     attackValue = "";
     counterAttackValue = "";
-    // wizardOptions = [
-    // {name: "Harry Potter", health: 175, attackPower: 15, counterAttackPower: 25}, 
-    // {name: "Hermione Granger", health: 180, attackPower: 12, counterAttackPower: 22},
-    // {name: "Lord Voldemort", health: 120, attackPower: 10, counterAttackPower: 20},
-    // {name: "Severus Snape", health: 110, attackPower: 8, counterAttackPower: 18}
-    // ]; 
+    
+    harry = {
+        name: "Harry Potter",
+        health: 175, 
+        attackPower: 15, 
+        counterAttackPower: 25
+    };
+    
+    hermione = {
+        name: "Hermione Granger",
+        health: 180, 
+        attackPower: 12, 
+        counterAttackPower: 22
+    };
+    
+    voldemort = {
+        name: "Lord Voldemort",
+        health: 120, 
+        attackPower: 10, 
+        counterAttackPower: 20
+    };
+    
+    snape = {
+        name: "Severus Snape",
+        health: 110, 
+        attackPower: 8, 
+        counterAttackPower: 18
+    };
 
     $("#harry-health").text(harry.health);
     $("#hermione-health").text(hermione.health);
@@ -85,7 +111,8 @@ $(initialize());
 
 $("#js-harry").on("click", function() {
     if(wizardChosen === false && challengerChosen === false) {
-        wizardChosen = true; 
+        wizardChosen = true;
+        currentWizard = true;  
         $("#chosen-wizard").append($(this));
         $(this).attr("w-ap", harry.attackPower);
         $(".w-attack-power").text(harry.attackPower);
@@ -97,6 +124,7 @@ $("#js-harry").on("click", function() {
         counterAttackValue = parseInt(counterAttackValue);
     } else if (wizardChosen === true && challengerChosen === false) {
         challengerChosen = true; 
+        currentChallenger = true;
         $("#chosen-challenger").append($(this));
         $(this).attr("c-ap", harry.attackPower);
         $(".c-attack-power").text(harry.attackPower);
@@ -112,6 +140,7 @@ $("#js-harry").on("click", function() {
 $("#js-hermione").on("click", function() {
     if(wizardChosen === false && challengerChosen === false) {
         wizardChosen = true; 
+        currentWizard = true;
         $("#chosen-wizard").append($(this));
         $(this).attr("w-ap", hermione.attackPower);
         $(".w-attack-power").text(hermione.attackPower);
@@ -122,7 +151,8 @@ $("#js-hermione").on("click", function() {
         counterAttackValue = ($(this).attr("w-cap"));
         counterAttackValue = parseInt(counterAttackValue);
     } else if (wizardChosen === true && challengerChosen === false) {
-        challengerChosen = true; 
+        challengerChosen = true;
+        currentChallenger = true; 
         $("#chosen-challenger").append($(this));
         $(this).attr("c-ap", hermione.attackPower);
         $(".c-attack-power").text(hermione.attackPower);
@@ -138,6 +168,7 @@ $("#js-hermione").on("click", function() {
 $("#js-voldemort").on("click", function() {
     if(wizardChosen === false && challengerChosen === false) {
         wizardChosen = true; 
+        currentWizard = true;
         $("#chosen-wizard").append($(this));
         $(this).attr("w-ap", voldemort.attackPower);
         $(".w-attack-power").text(voldemort.attackPower);
@@ -149,6 +180,7 @@ $("#js-voldemort").on("click", function() {
         counterAttackValue = parseInt(counterAttackValue);
     } else if (wizardChosen === true && challengerChosen === false) {
         challengerChosen = true; 
+        currentChallenger = true;
         $("#chosen-challenger").append($(this));
         $(this).attr("c-ap", voldemort.attackPower);
         $(".c-attack-power").text(voldemort.attackPower);
@@ -164,6 +196,7 @@ $("#js-voldemort").on("click", function() {
 $("#js-snape").on("click", function() {
     if(wizardChosen === false && challengerChosen === false) {
         wizardChosen = true; 
+        currentWizard = true;
         $("#chosen-wizard").append($(this));
         $(this).attr("w-ap", snape.attackPower);
         $(".w-attack-power").text(snape.attackPower);
@@ -175,6 +208,7 @@ $("#js-snape").on("click", function() {
         counterAttackValue = parseInt(counterAttackValue);
     } else if (wizardChosen === true && challengerChosen === false) {
         challengerChosen = true; 
+        currentChallenger = true; 
         $("#chosen-challenger").append($(this));
         $(this).attr("c-ap", snape.attackPower);
         $(".c-attack-power").text(snape.attackPower);
@@ -187,25 +221,44 @@ $("#js-snape").on("click", function() {
     }
 });
 
-function handleExpelliarmus() {
-    $("#expelliarmus").on("click", function(){
-        if (wizardChosen === false) {
-         $("#current-status").text("Select a wizard");
-        } else if (challengerChosen === false) {
-            $("#current-status").text("Select a challenger");
-        } else {
-            expelliarmusSound.play();
-            $("#current-status").text("");
-            $(".duel-image").prepend($("<img>", {id: "duel-wand-image", src: "assets/images/duel-wand-image.png"}));
-        }
-    });
-}
+// function handleExpelliarmus() {
+//     $("#expelliarmus").on("click", function(){
+//         if (wizardChosen === false) {
+//          $("#current-status").text("Select a wizard");
+//         } else if (challengerChosen === false) {
+//             $("#current-status").text("Select a challenger");
+//         // } else {
+//         //     $("#current-status").text("");
+//             // $(".duel-image").prepend($("<img>", {id: "duel-wand-image", src: "assets/images/duel-wand-image.png"}));
+//         }
+        
+//     });
+// }
 
-$(handleExpelliarmus());
+// $(handleExpelliarmus());
+
+//maybe need to merge all of handleExpellarimus into duel 
 
 function duel() {
-    
+    if (wizardChosen === true && challengerChosen === true && currentWizard === true) {
+        $("#expelliarmus").on("click", function(){
+            $("#current-status").text("");
+            expelliarmusSound.play();
+            $("#chosen-wizard").find(".w-attack-power");
+            attackValue += attackValue;
+            $(".w-attack-power").text(attackValue);
+        });
+    } else if (wizardChosen === true && challengerChosen === true && currentChallenger === true) {
+        $("#expelliarmus").on("click", function(){
+            $("#current-status").text("");
+            expelliarmusSound.play();
+            counterAttackValue += counterAttackValue;
+            $(".c-counter-attack-power").text(counterAttackValue);
+        });
+    }
 }
+
+$(duel());
 
 function didYouWin() {
 
